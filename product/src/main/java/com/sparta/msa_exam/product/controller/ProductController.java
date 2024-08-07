56e3a1d7d0ccdd2baedb7433eeed0ea33211a322
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-08-08 00:06:22 +0900

    feat: Product 구현

---
+++ product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.sparta.msa_exam.product.controller;
+
+import com.sparta.msa_exam.product.dto.ProductDto;
+import com.sparta.msa_exam.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ProductDto create(@RequestBody ProductDto productDto) {
+        return productService.create(productDto);
+    }
+
+    @GetMapping
+    public List<ProductDto> getProducts() {
+        return productService.getProducts();
+    }
+
+
+}
