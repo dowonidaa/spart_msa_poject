ef7a52119d44a5d46e9e25558a37fbe875d3e2cd
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-08-08 13:04:17 +0900

    feat: order ResponseHeader에 ServerPort 추가 aop 구현

---
+++ order/src/main/java/com/sparta/msa_exam/order/aop/ServerPortAspect.java
@@ -0,0 +1,23 @@
+package com.sparta.msa_exam.order.aop;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class ServerPortAspect {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @AfterReturning(value = "execution(* com.sparta.msa_exam.order.controller.OrderController.*(..))", returning = "response")
+    public void addServerPortHeader(HttpServletResponse response) {
+        log.info("ServerPortAspect!!");
+        response.setHeader("Server_Port", serverPort);
+    }
+}
